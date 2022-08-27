c60b8709271c56e53ce205172b8c3bcb8f6c54eb
AbhijitMahajan141 <abhimahajan142001@gmail.com>
2022-08-27 14:13:22 +0530

    Second commit

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -2,13 +2,23 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract CrowdFunding {
-    mapping(address => uint256) public contributors;
+    mapping(address => uint256) public contributors; //this means that address will hold a value of tyoe uint which in ourcase is ether or wei
     address public manager;
     uint256 public minContribution;
     uint256 public deadline;
     uint256 public target;
     uint256 public raisedAmount;
     uint256 public noOfContributors;
+    struct Request {
+        string description;
+        address payable recipient;
+        uint256 value;
+        bool completed;
+        uint256 noOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint256 => Request) public requests;
+    uint256 public numRequests;
 
     constructor(uint256 _target, uint256 _deadline) {
         target = _target;
@@ -25,6 +35,8 @@ contract CrowdFunding {
             "Minimum contribution is not met!"
         );
         if (contributors[msg.sender] == 0) {
+            //This if check is to check if the contibutor is sending first time or not if he is sending first time we increment value of noOfContributors.
+            //if the same contributor tries to send small amounts of money we dont want to increment the contributors.
             noOfContributors++;
         }
         contributors[msg.sender] += msg.value;
@@ -33,5 +45,61 @@ contract CrowdFunding {
 
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function refund() public {
+        require(
+            block.timestamp > deadline && raisedAmount < target,
+            "You are not eligible for refund yet"
+        );
+        require(contributors[msg.sender] > 0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]); //contributors[msg.sender] means whatever that address holds refund that much amount
+        contributors[msg.sender] = 0; //resseting the account after refund
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only Manager can call this function");
+        _;
+    }
+
+    function createRequests(
+        string memory _description,
+        address payable _recipient,
+        uint256 _value
+    ) public onlyManager {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    function voteRequest(uint256 _requestNo) public {
+        require(contributors[msg.sender] > 0, "You Must be a contributor");
+        Request storage thisRequest = requests[_requestNo];
+        require(
+            thisRequest.voters[msg.sender] == false,
+            "You have already voted!"
+        );
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint256 _requestNo) public onlyManager {
+        require(raisedAmount >= target);
+        Request storage thisRequest = requests[_requestNo];
+        require(
+            thisRequest.completed == false,
+            "The request is already Completed!"
+        );
+        require(
+            thisRequest.noOfVoters > noOfContributors / 2,
+            "Majority does not support!"
+        );
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
     }
 }
